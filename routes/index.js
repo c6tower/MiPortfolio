1d8babf9f58b0df620352093bfa926a3501f9160
mi <clock2elizabeth@gmail.com>
2020-03-02 17:57:03 +0900

    test design index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* INDEX page */
+router.get('/', function(req, res) {
+  const mainContents = [
+    ['WorkA', 'description'],
+    ['WorkB', 'description'],
+    ['WorkC', 'description'],
+    ['WorkD', 'description'],
+    ['WorkE', 'description'],
+    ['WorkF', 'description'],
+  ]
+  res.render('index', {
+    title: 'Express',
+    mainContents: mainContents
+  });
 });
 
 module.exports = router;
